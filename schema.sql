f141a61ab5e1c5cb730b50a27dd602f9227ae34a
Roger <rgbyford@gmail.com>
2018-11-27 21:02:06 -0600

    schema

--- schema.sql
+++ schema.sql
@@ -2,3 +2,17 @@
 DROP DATABASE IF EXISTS todolist;
 -- Creates the "todolist" database --
 CREATE DATABASE todolist;
+USE todolist;
+CREATE TABLE todo (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    todo VARCHAR(80) NOT NULL,
+    accomplished BOOLEAN DEFAULT false,
+    createdAt DATETIME,
+    updatedAt DATETIME
+);
+INSERT INTO todo (todo, accomplished, createdAt, updatedAt) 
+VALUES ('Make dinner', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO todo (todo, accomplished, createdAt, updatedAt) 
+VALUES ('Dishes', false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO todo (todo, accomplished, createdAt, updatedAt) 
+VALUES ('Eat', false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
